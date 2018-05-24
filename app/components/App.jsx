becfa45fa6a34a17fe08b2b3611d64603b4c0255
sudarshan955 <sudarshan955@gmail.com>
2018-05-24 02:51:02 +0000

    Create Menu Done
    Create Menu Done

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -25,6 +25,7 @@ export default class App extends React.Component {
         };
 
         this.addNote = this.addNote.bind(this);
+        this.findNote = this.findNote.bind(this);
         this.editNote = this.editNote.bind(this);
     }
 
@@ -34,7 +35,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <button onClick={this.addNote}>+</button>
-                <Notes items={notes} onEdit={this.editNote} />
+                <Notes items={notes} onEdit={this.editonEditNote} />
             </div>
         );
     }
@@ -48,7 +49,17 @@ export default class App extends React.Component {
         });
     }
 
+    findNote(noteId) {
+        let notes = this.state.notes;
+        return notes.findIndex( ( note ) => note.id == noteId );
+    }
+
     editNote(noteId, task) {
+        let notes = this.state.notes;
+        const noteIndex = this.findNote( noteId );
+        console.log( noteIndex );
+        notes[noteIndex].task = task;
+        this.setState( {notes} );
         console.log('note edited', noteId, task);
     }
 }
